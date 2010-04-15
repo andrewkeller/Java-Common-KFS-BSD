e4c0500df9ff521769ace20b11e6bf242beee63b
Andrew Keller <andrew@kellerfarm.com>
2010-04-15 14:42:16 -0400

    Began writing test cases in TestLinearArgDesequencer involving subclasses of LinearArgDesequencer.

--- src/com/kfs/bsd/common/util/junit/TestLinearArgDesequencer.java
+++ src/com/kfs/bsd/common/util/junit/TestLinearArgDesequencer.java
@@ -12,6 +12,44 @@ import com.kfs.bsd.common.util.LinearArgDesequencer;
 
 public class TestLinearArgDesequencer extends TestCase {
 	
+	// LinearArgDesequencer is intended to be used via
+	// a subclass, so we will need some for testing.
+	
+	protected class LADsEmpty extends LinearArgDesequencer {
+		
+		// This is a subclass of LinearArgDesequencer
+		// that does not define any arguments.
+		
+		public LADsEmpty() {
+			
+			// Java requires that this be here.
+			
+			// Forward the request to super.
+			
+			super();
+		}
+		
+		public LADsEmpty( String [] args ) {
+			
+			// Java requires that this be here.
+			
+			// Forward the request to super.
+			
+			super( args );
+		}
+		
+		protected void populateArgSet() {
+			
+			// Overriding super.populateArgSet().
+			
+			// Do not add any arguments, and do not call
+			// super.populateArgSet(), which would add
+			// the default arguments, help and verbose.
+		}
+	}
+	
+	// Test cases on LinearArgDesequencer directly
+	
 	public void testNoSubclassNoArgs() {
 		
 		LinearArgDesequencer args = new LinearArgDesequencer();
@@ -98,6 +136,33 @@ public class TestLinearArgDesequencer extends TestCase {
 		
 		verifyBasics( args, false, 2, false, 0 );
 	}
+	
+	// Test cases on LADsEmpty (a subclass of LinearArgDesequencer)
+	
+	public void testLADsEmptyNoArgs() {
+		
+		LADsEmpty args = new LADsEmpty();
+		
+		verifyBasics( args, false, 0, false, 0 );
+	}
+	
+	public void testLADsEmptyInvalidArgs1() {
+		
+		String [] cmdl_args = { "--help" };
+		LADsEmpty args = new LADsEmpty( cmdl_args );
+		
+		verifyBasics( args, false, 0, true, args.kErrorCodeUnknownArgument );
+	}
+	
+	public void testLADsEmptyInvalidArgs2() {
+		
+		String [] cmdl_args = { "--verbose", "--verbose" };
+		LADsEmpty args = new LADsEmpty( cmdl_args );
+		
+		verifyBasics( args, false, 0, true, args.kErrorCodeUnknownArgument );
+	}
+	
+	// Common testing code
 	
 	public void verifyBasics( LinearArgDesequencer lad, boolean help, int verbosity, boolean error, int errorCode ) {
 		
